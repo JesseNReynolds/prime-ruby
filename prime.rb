b4fb5a8e9bfc46e74fa9df72ea77eac478305eb2
JesseNReynolds <jessenreynolds@gmail.com>
2020-10-05 11:18:53 -0600

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,25 @@
-# Add  code here!
+def prime?(n)
+    truthy = []
+
+    if n < 2
+        return false
+
+    elsif n == 2
+        return true
+
+    else
+        (2...n).each do |i|
+            if n % i == 0
+                truthy << i
+            end
+    end
+
+    if truthy.length > 0
+        puts truthy
+        return false
+    else
+        puts truthy
+        return true
+    end
+end
+end
